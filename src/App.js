43071548d9dc834d478291497600a2de66378f68
GRECO <greco@air-de-greco-2.lan>
2022-05-03 11:53:28 +0200

    first-day

--- src/App.js
+++ src/App.js
@@ -4,11 +4,39 @@ import { useEffect, useState } from "react";
 
 import axios from "axios";
 import Deliveroo from "../src/img/Deliveroo.png";
-import { specialCharMap } from "@testing-library/user-event/dist/keyboard";
+import Knife from "../src/img/knife.jpeg";
 
 function App() {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  // const [counter, setCounter] = useState([0]);
+
+  // const newCounter = () => {
+  //   const newCounter = [...counter]; //copie du TB
+  //   newCounter.push(0); // modification de la copie
+  //   setCounter(newCounter); //Mise à jour du state avec la copie
+  // };
+
+  // const handleClickMinus = (index) => {
+  //   const newCounter = [...counter];
+  //   newCounter[index]--;
+  //   setCounter(newCounter);
+  // };
+
+  // const handkeClickPlus = (index) => {
+  //   const newCounter = [...counter];
+  //   newCounter[index]++;
+  //   setCounter(newCounter);
+  // };
+
+  // const [addingMeals, setAddingMeals] = useState([]);
+
+  // const addNewMeal = (event) => {
+  //   event.preventDefault();
+  //   const newMeal = [...addingMeals];
+  //   newMeal.push(categories.meals);
+  //   setAddingMeals(newMeal);
+  // };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -37,17 +65,39 @@ function App() {
 
       <div className="categories">
         {data.categories.map((categories, index) => {
-          return <h2 key={index}>{categories.name}</h2>;
+          const isArrayOfMealsEmpty = categories.meals.length;
+          return (
+            isArrayOfMealsEmpty > 0 && (
+              <div className="name-category" key={index}>
+                <h2>{categories.name}</h2>
+                <div
+                  className="details"
+                  onClick={() => {
+                    alert("Cliqué");
+                  }}
+                >
+                  {categories.meals.map((meals, index) => {
+                    return (
+                      <div className="image" key={index}>
+                        <h3> {meals.title}</h3>
+                        <p>{meals.description}</p>
+                        <p>{meals.price}</p>
+                        <div>{meals.popular ? <span>populaire</span> : ""}</div>
+                        <div className="meals-picture">
+                          {meals.picture ? (
+                            <img src={meals.picture} alt="menus images" />
+                          ) : (
+                            <img src={Knife} />
+                          )}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )
+          );
         })}
-        <div className="details">
-          {data.categories.meals.map((meals, index) => {
-            return <h3 key={index}>{meals.title}</h3>;
-          })}
-        </div>
-        {/* 
-        {data.categories.map((meals, index) => {
-          return <h3 key={index}>{categories.meals}</h3>;
-        })} */}
       </div>
     </div>
   );
